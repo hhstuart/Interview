b22fcad0cf6b48fbc4d6fd37709f6fe0a69ca94d
Harold Stuart <hhstuart@mac.com>
2022-07-18 20:12:09 -0600

    Initial Commit

---
+++ Interview/InterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewApp.swift
+//  Interview
+//
+//  Created by hhstuart on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct InterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
